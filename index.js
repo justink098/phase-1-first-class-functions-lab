7ed9397b967740d072e2fa3d58b439bc26505d15
Justin <justin.kiarie@student.moringaschool.com>
2022-08-19 10:21:58 +0300

    Completed assingment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+
+
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+const returnFirstTwoDrivers = function (drivers){
+    return (drivers.slice(0,2))
+}
+
+const returnLastTwoDrivers = function (drivers){
+    return (drivers.slice(-2))
+}
+
+
+const selectingDrivers = [returnFirstTwoDrivers , returnLastTwoDrivers];
+
+
+function createFareMultiplier(factor){
+    return nm1 =>  nm1* factor;
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(arrayOfDrivers, cb){
+    return cb(arrayOfDrivers)
+
+}
